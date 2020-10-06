85af977e1d4fc44a03b6301bf2884d67b0beb2de
18741847 <AOKulakov@sberbank.ru>
2020-10-06 10:42:43 +0300

    User entity done

---
+++ src/main/java/impl/EntityId.java
@@ -0,0 +1,20 @@
+package impl;
+
+public class EntityId {
+    private int id;
+
+    public EntityId() {
+    }
+
+    public EntityId(int id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
--- src/main/java/impl/User.java
+++ src/main/java/impl/User.java
@@ -1,6 +1,85 @@
 package impl;
 
-public class User {
+import java.util.List;
+import java.util.Objects;
+
+public class User extends EntityId {
     private String name;
     private int age;
+    private List<Account> accounts;
+
+    public User() {
+    }
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public User(String name, int age, List<Account> accounts) {
+        this.name = name;
+        this.age = age;
+        this.accounts = accounts;
+    }
+    public User(int id, String name, int age) {
+        super(id);
+        this.name = name;
+        this.age = age;
+    }
+
+    public User(int id, String name, int age, List<Account> accounts) {
+        super(id);
+        this.name = name;
+        this.age = age;
+        this.accounts = accounts;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(List<Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                ", accounts=" + accounts +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(name, user.name) &&
+                Objects.equals(age, user.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
 }
